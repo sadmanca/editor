e2c65f18cd98f027070269782955f36fc5b3a744
Sadman Hossain <m.sadman.h@gmail.com>
2024-11-18 23:44:27 -0500

    Update vendor.sh to use fork with sample example

--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -28,7 +28,7 @@ VEGA_LITE_VERSION=$(scripts/version.sh vega-lite)
 
 pushd /tmp
 curl https://github.com/vega/vega/archive/refs/tags/v$VEGA_VERSION.tar.gz -L -o vega.tar.gz
-curl https://github.com/vega/vega-lite/archive/refs/tags/v$VEGA_LITE_VERSION.tar.gz -L -o vl.tar.gz
+curl https://github.com/sadmanca/vega-lite/archive/refs/tags/v0.0.1.tar.gz -L -o vl.tar.gz
 tar xzf vega.tar.gz vega-$VEGA_VERSION/docs
 tar xzf vl.tar.gz vega-lite-$VEGA_LITE_VERSION/examples vega-lite-$VEGA_LITE_VERSION/site/_data
 popd
